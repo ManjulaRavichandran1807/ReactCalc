f9ca961ad31f218dab64c9b72a4d345dda6a1743
Manju0703 <manjularavichandran.7@gmail.com>
2021-12-22 16:12:15 +0530

    init

--- App.js
+++ App.js
@@ -1,111 +1,170 @@
-import React from "react";
-import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
-
-import Row from "./components/Row";
-import Button from "./components/Button";
-import calculator, { initialState } from "./util/calculator";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#202020",
-    justifyContent: "flex-end"
-  },
-  value: {
-    color: "#fff",
-    fontSize: 40,
-    textAlign: "right",
-    marginRight: 20,
-    marginBottom: 10
-  }
-});
+import React from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-  state = initialState;
+  constructor(){
+    super();
+    this.state={
+      resultText: "",
+      calculationText: ""
+    };
+    this.operations=['DEL','+','-','x','/'];
+  }
 
-  handleTap = (type, value) => {
-    this.setState(state => calculator(type, value, state));
-  };
+  calculateResult(){
+    const text = this.state.resultText
+    // do magic
+    this.setState({
+      calculationText: eval(text)
+    })
+  }
+  validate(){
+    const text = this.state.resultText
+    switch(text.slice(-1)){
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+      return false
+    }
+    return true
+  }
+  
+
+  buttonPressed(text){
+    if(text == '='){
+      return this.validate() &&  this.calculateResult()
+    }
+    this.setState({
+      resultText: this.state.resultText+text
+    })
+  }
+
+  operate(operation){
+    switch(operation) {
+      case 'DEL':
+              let text = this.state.resultText.split('')
+              text.pop()
+              this.setState({
+                resultText: text.join('')
+              })
+              break
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+              const lastChar = this.state.resultText.split('').pop()
+
+              if(this.operations.indexOf(lastChar) >0) return
+
+              if(this.state.resultText ==""){
+                return
+              }
+              this.setState({
+                resultText: this.state.resultText+operation
+              })
+
+    }
+  }
+
+  
 
   render() {
+    let rows =[];
+    let nums =[[1,2,3],[4,5,6],[7,8,9],['.',0,'=']];
+    for(let i=0; i<4;i++){
+      let row =[];
+      for(let j=0; j<3 ;j++){
+        row.push(<TouchableOpacity key={nums[i][j]} style={styles.btn}>
+                    <Text onPress={() => this.buttonPressed(nums[i][j])} style={styles.btntext}>{nums[i][j]}</Text>
+                 </TouchableOpacity>);
+      }
+      rows.push(<View key ={i} style={styles.row}>{row}</View>);
+    }
+
+    let ops = [];
+    for(let i=0; i<5; i++){
+      ops.push(<TouchableOpacity key={this.operations[i]} style={styles.btn} onPress={() => this.operate(this.operations[i])}>
+        <Text style={[styles.btntext, styles.white]}>{this.operations[i]}</Text>
+     </TouchableOpacity>)
+    } 
+
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <SafeAreaView>
-          <Text style={styles.value}>
-            {parseFloat(this.state.currentValue).toLocaleString()}
-          </Text>
-          <Row>
-            <Button
-              text="C"
-              theme="secondary"
-              onPress={() => this.handleTap("clear")}
-            />
-            <Button
-              text="+/-"
-              theme="secondary"
-              onPress={() => this.handleTap("posneg")}
-            />
-            <Button
-              text="%"
-              theme="secondary"
-              onPress={() => this.handleTap("percentage")}
-            />
-            <Button
-              text="/"
-              theme="accent"
-              onPress={() => this.handleTap("operator", "/")}
-            />
-          </Row>
-
-          <Row>
-            <Button text="7" onPress={() => this.handleTap("number", 7)} />
-            <Button text="8" onPress={() => this.handleTap("number", 8)} />
-            <Button text="9" onPress={() => this.handleTap("number", 9)} />
-            <Button
-              text="x"
-              theme="accent"
-              onPress={() => this.handleTap("operator", "*")}
-            />
-          </Row>
-
-          <Row>
-            <Button text="4" onPress={() => this.handleTap("number", 4)} />
-            <Button text="5" onPress={() => this.handleTap("number", 5)} />
-            <Button text="6" onPress={() => this.handleTap("number", 6)} />
-            <Button
-              text="-"
-              theme="accent"
-              onPress={() => this.handleTap("operator", "-")}
-            />
-          </Row>
-
-          <Row>
-            <Button text="1" onPress={() => this.handleTap("number", 1)} />
-            <Button text="2" onPress={() => this.handleTap("number", 2)} />
-            <Button text="3" onPress={() => this.handleTap("number", 3)} />
-            <Button
-              text="+"
-              theme="accent"
-              onPress={() => this.handleTap("operator", "+")}
-            />
-          </Row>
-
-          <Row>
-            <Button
-              text="0"
-              size="double"
-              onPress={() => this.handleTap("number", 0)}
-            />
-            <Button text="." onPress={() => this.handleTap("number", ".")} />
-            <Button
-              text="="
-              theme="accent"
-              onPress={() => this.handleTap("equal")}
-            />
-          </Row>
-        </SafeAreaView>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>{this.state.resultText}</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>{this.state.calculationText}</Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>
+            {rows}
+          </View>
+          <View style={styles.operations}>
+            {ops}
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    row: {
+      flexDirection: 'row',
+      flex: 1,
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    },
+    btntext: {
+      fontSize: 30,
+
+    },
+    white: {
+      color: 'white'
+    },
+    btn: {
+      flex: 1,
+      alignItems: 'center',
+      alignSelf: 'stretch',
+      justifyContent: 'center'
+    },
+    calculationText: {
+      fontSize: 24,
+      color: 'white',        
+    },
+    resultText: {
+      fontSize: 30,
+      color: 'white',
+    },
+    result: {
+      flex: 2,
+      backgroundColor: '#8bc2f9',
+      justifyContent: 'center',
+      alignItems: 'flex-end'
+    },
+    calculation: {
+      flex: 1,
+      backgroundColor: '#4ba0f4',
+      justifyContent: 'center',
+      alignItems: 'flex-end'
+    },
+    buttons: {
+      flex: 7,
+      flexDirection: 'row'
+    },
+    numbers: {
+      flex: 3,
+      backgroundColor: '#82868c'
+    },
+    operations: {
+      flex: 1,
+      justifyContent: 'space-around',
+      alignItems: 'stretch',
+      backgroundColor: '#b8babc'
+    }
+});
